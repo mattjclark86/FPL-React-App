13ee6a82b24306cf7aa3543b75d1f9406d28cb0d
Matthew <mattjclark86@yahoo.co.uk>
2020-11-14 15:21:55 +0000

    Added Menu Fundamentals

--- fpl-app/src/index.js
+++ fpl-app/src/index.js
@@ -1,12 +1,69 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Link, Switch } from "react-router-dom";
 import './index.css';
-import App from './App';
+import './App.css';
 import reportWebVitals from './reportWebVitals';
+
+class AppOne extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="App">
+          Item List
+          <Link to="/itemOne" className="menuLink">Item One</Link>
+          <Link to="/itemTwo" className="menuLink">Item Two</Link>
+          <Link to="/itemThree" className="menuLink">Item Three</Link>
+          <Switch>
+            <Route exact path="/itemOne">
+              <ItemOne />
+            </Route>
+            <Route exact path="/itemTwo">
+              <ItemTwo />
+            </Route>
+            <Route exact path="/itemThree">
+              <ItemThree />
+            </Route>
+          </Switch>
+        </div>
+      </BrowserRouter>
+    );
+  }
+}
+
+class ItemOne extends Component {
+  render() {
+    return (
+      <div className="App">
+        Item 1
+      </div>
+    );
+  }
+}
+
+class ItemTwo extends Component {
+  render() {
+    return (
+      <div className="App">
+        Item 2
+      </div>
+    );
+  }
+}
+
+class ItemThree extends Component {
+  render() {
+    return (
+      <div className="App">
+        Item 3
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <AppOne />
   </React.StrictMode>,
   document.getElementById('root')
 );
