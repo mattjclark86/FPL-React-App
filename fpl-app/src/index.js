60d4ccb49712f3b5408c10fe4a4aedd7e7b37c17
Matthew <mattjclark86@yahoo.co.uk>
2020-11-15 16:14:21 +0000

    Made App width/height flexible, added antd components

--- fpl-app/src/index.js
+++ fpl-app/src/index.js
@@ -1,32 +1,52 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route, Link, Switch } from "react-router-dom";
-import './index.css';
+import { Layout } from 'antd';
+import 'antd/dist/antd.css';
 import './App.css';
+//Above import is a temporary font color solution until
+//I figure out how to edit the antd css
 import reportWebVitals from './reportWebVitals';
+
+//Tasks for next time:
+//Fit the content/footer/sider (and header) better, make them central and size flexible
+//Figure out how to properly style the components using antd css
 
 class AppOne extends Component {
   render() {
     return (
-      <BrowserRouter>
-        <div className="App">
-          Item List
-          <Link to="/itemOne" className="menuLink">Item One</Link>
-          <Link to="/itemTwo" className="menuLink">Item Two</Link>
-          <Link to="/itemThree" className="menuLink">Item Three</Link>
-          <Switch>
-            <Route exact path="/itemOne">
-              <ItemOne />
-            </Route>
-            <Route exact path="/itemTwo">
-              <ItemTwo />
-            </Route>
-            <Route exact path="/itemThree">
-              <ItemThree />
-            </Route>
-          </Switch>
-        </div>
-      </BrowserRouter>
+      <Layout className="Layout">
+        <BrowserRouter>
+          <Layout.Header className="Header">Item List (Header)</Layout.Header>
+          <Layout>
+            <Layout.Sider className="Sider" width="250px">
+              <ol>
+                <li>
+                  <Link to="/itemOne" className="menuLink">Item One (First Link)</Link>
+                </li>
+                <li>
+                  <Link to="/itemTwo" className="menuLink">Item Two (Second Link)</Link>
+                </li>
+                <li>
+                  <Link to="/itemThree" className="menuLink">Item Three (Third Link)</Link>
+                </li>
+              </ol>
+            </Layout.Sider>
+            <Layout.Footer className="Footer">FPL App (Footer)</Layout.Footer>
+            <Switch>
+              <Route exact path="/itemOne">
+                <ItemOne />
+              </Route>
+              <Route exact path="/itemTwo">
+                <ItemTwo />
+              </Route>
+              <Route exact path="/itemThree">
+                <ItemThree />
+              </Route>
+            </Switch>
+          </Layout>
+        </BrowserRouter>
+      </Layout>
     );
   }
 }
@@ -34,9 +54,9 @@ class AppOne extends Component {
 class ItemOne extends Component {
   render() {
     return (
-      <div className="App">
-        Item 1
-      </div>
+      <Layout>
+        <Layout.Content className="Content">Item 1 (Content)</Layout.Content>
+      </Layout>
     );
   }
 }
@@ -44,9 +64,9 @@ class ItemOne extends Component {
 class ItemTwo extends Component {
   render() {
     return (
-      <div className="App">
-        Item 2
-      </div>
+      <Layout>
+        <Layout.Content className="Content">Item 2 (Content)</Layout.Content>
+      </Layout>
     );
   }
 }
@@ -54,9 +74,9 @@ class ItemTwo extends Component {
 class ItemThree extends Component {
   render() {
     return (
-      <div className="App">
-        Item 3
-      </div>
+      <Layout>
+        <Layout.Content className="Content">Item 3 (Content)</Layout.Content>
+      </Layout>
     );
   }
 }
